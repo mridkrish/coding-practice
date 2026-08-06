4a32c8dd37f20320745df54fcdbbe0c6590aeb79
mridkrish <mridhul.chengat@gmail.com>
2026-08-06 23:10:28 +0530

    Initial commit

---
+++ Java/Pattern/Pattern18.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Pattern18 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+
+            for (int j=0;j<=i;j++){
+                System.out.print((char)('A' + n -1 - i + j));
+            }
+            
+            System.out.println();
+
+        }
+
+    }
+}
