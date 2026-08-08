3a25dd0e3fed758da2b6a8e0ef670f383dcfb72e
mridkrish <mridhul.chengat@gmail.com>
2026-08-08 22:29:45 +0530

    Pattern 20 Java code

---
+++ Pattern/Java/Pattern20.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Pattern20 {
+
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+
+            for(int j = 0; j < 2*(n-i-1); j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+
+        }
+        
+
+        for(int i = 0; i < n; i++){
+
+            for(int j=0; j < n-i; j++){
+                System.out.print("*");
+            }
+
+            for(int j=0; j <= 2*i-1; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 0; j < n-i; j++){
+                System.out.print("*");
+            }
+
+            System.err.println();
+        }
+    }
+}
